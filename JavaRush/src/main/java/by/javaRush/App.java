af0848c5e541ffa02a461f175482af4f62289b87
Utlik <UtlikAD@qulix.com>
2020-01-24 15:35:20 +0300

    add java project

---
+++ JavaRush/src/main/java/by/javaRush/App.java
@@ -0,0 +1,13 @@
+package by.javaRush;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
