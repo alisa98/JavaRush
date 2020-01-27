d0d72f8c4d0a732515e9b69acaacbad0b52765e6
Utlik <UtlikAD@qulix.com>
2020-01-27 15:48:01 +0300

    add function minimum of three number

--- JavaRush/src/main/java/by/javaRush/tasks/task02/task0216/Solution.java
+++ JavaRush/src/main/java/by/javaRush/tasks/task02/task0216/Solution.java
@@ -1,4 +1,25 @@
 package by.javaRush.tasks.task02.task0216;
 
 public class Solution {
+    public static void main(String[] args){
+        System.out.println(min(1, 2, 3));
+        System.out.println(min(2, 1, 3));
+        System.out.println(min(2, 3, 1));
+        System.out.println(min(1, 1, 2));
+        System.out.println(min(1, 2, 1));
+        System.out.println(min(2, 1, 1));
+        System.out.println(min(1, 1, 1));
+    }
+
+    public static int min(int a, int b, int c)
+    {
+        if(a < b && a < c) return a;
+             else if ( a > c) return c;
+                else if ( a == b && a <= c) return a;
+                 else if ( a == c && a < b)  return a;
+        if (a > b && b < c ) return b;
+            else if (a > b && b == c) return b;
+        if ( a > b && b > c) return c;
+        return 0;
+    }
 }
