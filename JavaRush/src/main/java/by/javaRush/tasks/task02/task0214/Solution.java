06afdfe70aaa736211f2997ef61aee02ca2e5a43
Utlik <UtlikAD@qulix.com>
2020-01-27 15:47:23 +0300

    add function minimum of two number

--- JavaRush/src/main/java/by/javaRush/tasks/task02/task0214/Solution.java
+++ JavaRush/src/main/java/by/javaRush/tasks/task02/task0214/Solution.java
@@ -1,11 +1,11 @@
-package by.javaRush.task02.task0214;
+package by.javaRush.tasks.task02.task0214;
 
 public class Solution {
 
     public static void main(String[] args){
         System.out.println(min(4,5));
         System.out.println(min(30,-3));
-        System.out.println(min(0,0));
+        System.out.println(min(1,1));
 
     }
 
