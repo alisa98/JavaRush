ccc0fe47a66152018a678bfeb8ce19cc6f2d1b2a
Utlik <UtlikAD@qulix.com>
2020-01-27 16:26:15 +0300

    add function minimum of 4 number

---
+++ JavaRush/src/main/java/by/javaRush/tasks/task02/Solution.java
@@ -0,0 +1,25 @@
+package by.javaRush.tasks.task02;
+
+
+public class Solution {
+    public static void main(String[] args)
+    {
+        System.out.println(min(1, 2, 3, 4));
+        System.out.println(min(2, 1, 4, 3));
+        System.out.println(min(1, 1, 3, 3));
+        System.out.println(min(1, 1, 1, 1));
+    }
+
+    public static int min(int a, int b, int c, int d){
+        if( min(a,b) < min(c,d)) return min(a,b);
+            else if (min(a,b) == min(d,c)) return min(a,b);
+        else return min(a,b);
+    }
+
+    public static int min(int a, int b){
+        if (a<=b)
+            return a;
+        else
+            return b;
+    }
+}
