6a4ca3596ea1c088033aa0dac16f2a09e16d61d6
Utlik <UtlikAD@qulix.com>
2020-01-28 13:29:04 +0300

    add bufferReader

--- JavaRush/src/main/java/by/javaRush/tasks/task03/task0316/Escape.java
+++ JavaRush/src/main/java/by/javaRush/tasks/task03/task0316/Escape.java
@@ -1,4 +1,16 @@
 package by.javaRush.tasks.task03.task0316;
 
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
 public class Escape {
+    public static void main(String[] args) {
+        System.out.println("It's Windows path: \"C:\\Program Files\\Java\\jdk1.7.0\\bin\"");
+        System.out.println("It's Java string: \\\"C:\\\\Program Files\\\\Java\\\\jdk1.7.0\\\\bin\"");
+    }
+
 }
+
--- JavaRush/src/main/java/by/javaRush/tasks/task03/task0318/BufferReader.java
+++ JavaRush/src/main/java/by/javaRush/tasks/task03/task0318/BufferReader.java
@@ -1,4 +1,20 @@
 package by.javaRush.tasks.task03.task0318;
 
+import java.io.*;
+
 public class BufferReader {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Input name");
+        String name = bufferedReader.readLine(); //читаем строку с клавиатуры
+
+        System.out.println("Input age");
+        String sAge = bufferedReader.readLine(); //читаем строку с клавиатуры
+
+        int nAge = Integer.parseInt(sAge); //преобразовываем строку в число.
+
+        System.out.println(name + " захватит мир через " + nAge +" лет. Му-ха-ха!");
+    }
 }
